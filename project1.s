eac13e6d3289e06be1fbc98290396e7382edcf14
muradabdi <me@muradabdi.com>
2021-10-30 17:38:21 -0500

    Syscall to get text from input

--- project1.s
+++ project1.s
@@ -10,3 +10,8 @@
         li $v0, 4					# 4 = code to print string
         la $a0, message_in			# Load address of input message to print
         syscall						# Register $v0 contains number code of the system call
+        
+		li $v0, 8					# 8 = code to read string
+		la $a0, userInput			# Get text from input
+		li $a1, 12					# Load 12 into $a1
+		syscall 					
