f8eab86a62a913dd1ce53dfca3c27b8911b51453
muradabdi <me@muradabdi.com>
2021-10-30 17:23:11 -0500

    Set length of userInput, I/O messages to be printed

--- project1.s
+++ project1.s
@@ -1,6 +1,9 @@
 # PROGRAM: CSCI 201 MIPS Programming Project 1
 
 .data
+    message_in: .asciiz "Input: "  	# Input message
+	message_out: .asciiz "Output: " # Output message
+    userInput: .space 12 			# Set input character limit
 
 .text
     main:							# Start of code section
