b21aa3b19ac8ffefa70fb907449a73587d601d85
muradabdi <me@muradabdi.com>
2021-10-30 17:35:53 -0500

    Syscall to print input message string

--- project1.s
+++ project1.s
@@ -1,9 +1,12 @@
 # PROGRAM: CSCI 201 MIPS Programming Project 1
 
 .data
-    message_in: .asciiz "Input: "  	# Input message
-	message_out: .asciiz "Output: " # Output message
-    userInput: .space 12 			# Set input character limit
+	message_in: .asciiz "Input: "	# Input message
+	message_out: .asciiz "Output: "	# Output message
+	userInput: .space 12			# Set input character limit
 
 .text
-    main:							# Start of code section
+	main:							# Start of code section
+        li $v0, 4					# 4 = code to print string
+        la $a0, message_in			# Load address of input message to print
+        syscall						# Register $v0 contains number code of the system call
